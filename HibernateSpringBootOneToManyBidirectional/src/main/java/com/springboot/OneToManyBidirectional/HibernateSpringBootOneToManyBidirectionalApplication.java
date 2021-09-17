25ae05550b1357594e84a0d7224f910c175edca7
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-17 09:51:41 -0300

    item-1 init

---
+++ HibernateSpringBootOneToManyBidirectional/src/main/java/com/springboot/OneToManyBidirectional/HibernateSpringBootOneToManyBidirectionalApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.OneToManyBidirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringBootOneToManyBidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringBootOneToManyBidirectionalApplication.class, args);
+	}
+
+}
---
+++ HibernateSpringBootOneToManyBidirectional/src/test/java/com/springboot/OneToManyBidirectional/HibernateSpringBootOneToManyBidirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.OneToManyBidirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringBootOneToManyBidirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
