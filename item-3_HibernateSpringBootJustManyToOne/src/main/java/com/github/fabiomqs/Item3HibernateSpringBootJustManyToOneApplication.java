6e187ea43bb5e96477e3611b747084701f67751d
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 16:52:13 -0300

    item3

---
+++ item-3_HibernateSpringBootJustManyToOne/src/main/java/com/github/fabiomqs/Item3HibernateSpringBootJustManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item3HibernateSpringBootJustManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item3HibernateSpringBootJustManyToOneApplication.class, args);
+	}
+
+}
---
+++ item-3_HibernateSpringBootJustManyToOne/src/test/java/com/github/fabiomqs/Item3HibernateSpringBootJustManyToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item3HibernateSpringBootJustManyToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
