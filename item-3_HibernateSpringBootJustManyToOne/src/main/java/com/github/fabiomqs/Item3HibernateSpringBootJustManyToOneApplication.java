db577934334b713fb1b99312fe79e82db6b9986b
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 17:08:54 -0300

    item 3 done.

--- item-3_HibernateSpringBootJustManyToOne/src/main/java/com/github/fabiomqs/Item3HibernateSpringBootJustManyToOneApplication.java
+++ item-3_HibernateSpringBootJustManyToOne/src/main/java/com/github/fabiomqs/Item3HibernateSpringBootJustManyToOneApplication.java
@@ -1,13 +1,52 @@
 package com.github.fabiomqs;
 
+import com.github.fabiomqs.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Item3HibernateSpringBootJustManyToOneApplication {
 
+	private final BookstoreService bookstoreService;
+
+	public Item3HibernateSpringBootJustManyToOneApplication(BookstoreService bookstoreService) {
+		this.bookstoreService = bookstoreService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Item3HibernateSpringBootJustManyToOneApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner init() {
+		return args -> {
+			System.out.println("\nInsert new book to an author ...");
+			System.out.println("---------------------------------------------");
+			bookstoreService.insertNewBook();
+			System.out.println("---------------------------------------------");
+
+			System.out.println("\nList all books of an author ...");
+			System.out.println("---------------------------------------------");
+			bookstoreService.fetchBooksOfAuthorById();
+			System.out.println("---------------------------------------------");
+
+			System.out.println("\nList a page books (first page with two books) of an author ...");
+			System.out.println("---------------------------------------------");
+			bookstoreService.fetchPageBooksOfAuthorById();
+			System.out.println("---------------------------------------------");
+
+			System.out.println("\nFetch a list of books and add new book ...");
+			System.out.println("---------------------------------------------");
+			bookstoreService.fetchBooksOfAuthorByIdAndAddNewBook();
+			System.out.println("---------------------------------------------");
+
+			System.out.println("\nFetch a list of books and delete the first book ...");
+			System.out.println("---------------------------------------------");
+			bookstoreService.fetchBooksOfAuthorByIdAndDeleteFirstBook();
+			System.out.println("---------------------------------------------");
+		};
 	}
 
 }
---
+++ item-3_HibernateSpringBootJustManyToOne/src/main/java/com/github/fabiomqs/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.github.fabiomqs.repository;
+
+import com.github.fabiomqs.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
