14214e3a4994b24abf8c9d24fd6fe60fba400f7b
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 17:19:50 -0300

    item4

---
+++ item-4_HibernateSpringBootManyToManyBidirectional/src/main/java/com/github/fabiomqs/Item4HibernateSpringBootManyToManyBidirectionalApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item4HibernateSpringBootManyToManyBidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item4HibernateSpringBootManyToManyBidirectionalApplication.class, args);
+	}
+
+}
