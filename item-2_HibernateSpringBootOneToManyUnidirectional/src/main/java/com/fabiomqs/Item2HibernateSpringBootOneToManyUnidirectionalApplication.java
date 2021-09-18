672837ca1bb5e7a56de1969cf6400afb568cd6f7
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 10:05:36 -0300

    item2 init

---
+++ item-2_HibernateSpringBootOneToManyUnidirectional/src/main/java/com/fabiomqs/Item2HibernateSpringBootOneToManyUnidirectionalApplication.java
@@ -0,0 +1,13 @@
+package com.fabiomqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item2HibernateSpringBootOneToManyUnidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item2HibernateSpringBootOneToManyUnidirectionalApplication.class, args);
+	}
+
+}
---
+++ item-2_HibernateSpringBootOneToManyUnidirectional/src/test/java/com/fabiomqs/Item2HibernateSpringBootOneToManyUnidirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item2HibernateSpringBootOneToManyUnidirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
