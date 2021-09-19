e6da832bab984566415a3aefbfbab7a106272ec9
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-19 10:55:43 -0300

    item6

---
+++ item-6_HibernateSpringBootCascadeChildRemoval/src/main/java/om/github/fabiomqs/Item6HibernateSpringBootCascadeChildRemovalApplication.java
@@ -0,0 +1,13 @@
+package om.github.fabiomqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item6HibernateSpringBootCascadeChildRemovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item6HibernateSpringBootCascadeChildRemovalApplication.class, args);
+	}
+
+}
---
+++ item-6_HibernateSpringBootCascadeChildRemoval/src/test/java/om/github/fabiomqs/Item6HibernateSpringBootCascadeChildRemovalApplicationTests.java
@@ -0,0 +1,13 @@
+package om.github.fabiomqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item6HibernateSpringBootCascadeChildRemovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
