2fe34a3fcc44b1376dbc4091a9d24d903e462750
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-19 09:51:08 -0300

    item5.1

---
+++ item-5_HibernateSpringBootManyToManyBidirectionalListVsSet/src/main/java/com/github/fabiomqs/Item5HibernateSpringBootManyToManyBidirectionalListVsSetApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Item5HibernateSpringBootManyToManyBidirectionalListVsSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Item5HibernateSpringBootManyToManyBidirectionalListVsSetApplication.class, args);
+	}
+
+}
---
+++ item-5_HibernateSpringBootManyToManyBidirectionalListVsSet/src/test/java/com/github/fabiomqs/Item5HibernateSpringBootManyToManyBidirectionalListVsSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item5HibernateSpringBootManyToManyBidirectionalListVsSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
