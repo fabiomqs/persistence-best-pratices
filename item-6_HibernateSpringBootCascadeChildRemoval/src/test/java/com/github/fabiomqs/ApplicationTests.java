fea7b488f38eace295a75485fce4a5fa8a74f6a0
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-20 07:17:41 -0300

    rename package.

--- item-6_HibernateSpringBootCascadeChildRemoval/src/test/java/com/github/fabiomqs/ApplicationTests.java
+++ item-6_HibernateSpringBootCascadeChildRemoval/src/test/java/com/github/fabiomqs/ApplicationTests.java
@@ -1,10 +1,10 @@
-package om.github.fabiomqs;
+package com.github.fabiomqs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Item6HibernateSpringBootCascadeChildRemovalApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
