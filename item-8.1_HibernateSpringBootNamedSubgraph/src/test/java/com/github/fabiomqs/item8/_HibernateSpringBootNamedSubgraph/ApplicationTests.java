4a593c197c74c2cef4f382e039b594db938f55f3
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-20 08:10:27 -0300

    item 8

---
+++ item-8.1_HibernateSpringBootNamedSubgraph/src/test/java/com/github/fabiomqs/item8/_HibernateSpringBootNamedSubgraph/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs.item8._HibernateSpringBootNamedSubgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
