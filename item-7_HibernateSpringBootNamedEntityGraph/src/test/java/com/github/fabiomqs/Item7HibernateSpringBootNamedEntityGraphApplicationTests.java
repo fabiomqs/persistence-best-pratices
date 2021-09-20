ddf4b2fec6611bbb25426bd5b0d6efaa34ed43bc
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-20 07:19:21 -0300

    item 7.1

---
+++ item-7_HibernateSpringBootNamedEntityGraph/src/test/java/com/github/fabiomqs/Item7HibernateSpringBootNamedEntityGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabiomqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item7HibernateSpringBootNamedEntityGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
